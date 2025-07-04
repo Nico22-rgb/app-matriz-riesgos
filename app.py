9a2bcf561e23e66b812319358496ff5326171e39
Nico22-rgb <nibuitragoc@unal.edu.co>
2025-07-04 00:52:50 +0000

    Se agregó la pantalla de inicio

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 
-st.title("Generador de Matriz de Riesgos")
+# Subir el archivo
+archivo = st.file_uploader("Sube el archivo de la base de datos", [".xlsx"])
 
-st.write("### ¿Para qué tipo de validación desea hacer su análisis de riesgo?")
-opcion = st.radio(
-    "Seleccione una opción:",
-    ("Validación de proceso o envase", "Validación de limpieza")
-)
+if archivo:
 
-st.write(f"Ha seleccionado: **{opcion}**")
+    tipo_validacion = st.selectbox("¿Qué tipo de validación desea?", [
+                                   "Validacion 1", "Validacion 2", "Validacion 3"], index=None)
+
+    if tipo_validacion == "Validacion 1":
+        tipo_linea = st.selectbox("¿Qué linea desea consultar?", [
+                                  "Linea 1", "Linea 2", "Linea 3"], index=None)
+
+        if tipo_linea == "Linea 1":
+            etapas_seleccionadas = st.multiselect("Seleccione las etapas que apliquen", [
+                                                  "Green", "Yellow", "Red", "Blue"])

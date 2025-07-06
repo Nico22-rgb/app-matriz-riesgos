289bc59c5c9c26fd76f6212a286eaffd8b60d1a3
Nico22-rgb <nibuitragoc@unal.edu.co>
2025-07-06 00:40:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ if archivo:
                     # Diccionario de rangos por etapa (índices base 0)
                     rangos_por_etapa = {
                         "Dispensación": (1, 5),   # filas 2–6
-                        "Compresión": (6, 11),    # filas 7–11
+                        "Compresión": (6, 10),    # filas 7–11
                         "Fusión": (11, 16),
                         "Emulsión": (16, 21),
                         "Mezcla": (21, 26),

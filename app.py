9ecef8c564a3cd0153e574a653dd3b1e7021fada
Nico22-rgb <nibuitragoc@unal.edu.co>
2025-07-05 19:11:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,22 @@ with col2:
     st.image(imagen, width=300)  # Puedes ajustar el ancho según necesites
 
 # Subir el archivo
-archivo = st.file_uploader("Por favor sube el archivo de la base de datos de matrices de riesgo"", [".xlsx"])
+archivo = st.file_uploader("Sube el archivo de la base de datos 204", [".xlsx"])
+
+if archivo:
+
+    tipo_validacion = st.selectbox("¿Qué tipo de validación desea?", [
+                                   "Validacion 1", "Validacion 2", "Validacion 3"], index=None)
+
+    if tipo_validacion == "Validacion 1":
+        tipo_linea = st.selectbox("¿Qué linea desea consultar?", [
+                                  "Linea 1", "Linea 2", "Linea 3"], index=None)
+
+        if tipo_linea == "Linea 1":
+            etapas_seleccionadas = st.multiselect("Seleccione las etapas que apliquen", [
+                                                  "Green", "Yellow", "Red", "Blue"])
+
+
 
 
 

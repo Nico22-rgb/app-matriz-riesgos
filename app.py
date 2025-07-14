23c59f092ae80460a94fb73526005a3a85a8a0ce
Nico22-rgb <nibuitragoc@unal.edu.co>
2025-07-14 03:00:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -497,11 +497,11 @@ if st.session_state.get('area_roja_consultada', False):
         with col5:
             margen_error = st.select_slider("Margen de error (%)<2>", options=[1.0, 5.0])
 
-    # Notas explicativas justificadas debajo del expander
-    st.markdown("""
-        <div style='margin-top: 10px; font-size: 0.9em; color: #666; text-align: justify;'>
-            <p><sup>1</sup> Si no tienes acceso a datos históricos del valor de p, consulta el MIA y establece p=AQL (como número) para el atributo analizado. Si sabes que el proceso se encuentra bajo control para este atributo, puedes sugerir que p=AQL/2 ó p=AQL/3.</p>
-          
-            <p><sup>2</sup> El margen de error (E) representa cuánta precisión deseas tener al estimar la proporción de defectos (p), en otras palabras define la precisión estadística del muestreo. Si tu proceso es muy variable para este atributo, ó el atributo es un CQA, selecciona 1%, de lo contrario selecciona 5%.</p>
-            </div>
-    """, unsafe_allow_html=True)
+  st.markdown("""
+    <div style='margin-top: 10px; font-size: 0.9em; color: #666; text-align: justify;'>
+        <p><sup>1</sup> Si no tienes acceso a datos históricos del valor de <i>p</i>, consulta el MIA y establece <i>p = AQL</i> (como número) para el atributo analizado. Si sabes que el proceso se encuentra bajo control para este atributo, puedes sugerir que <i>p = AQL / 2</i> o <i>p = AQL / 3</i>.</p>
+
+        <p><sup>2</sup> El margen de error (<i>E</i>) representa cuánta precisión deseas tener al estimar la proporción de incumplimientos (<i>p</i>). En otras palabras, define la precisión estadística del muestreo. Si tu proceso es muy variable para este atributo, o el atributo es un CQA, selecciona 1%. De lo contrario, selecciona 5%.</p>
+    </div>
+""", unsafe_allow_html=True)
+
